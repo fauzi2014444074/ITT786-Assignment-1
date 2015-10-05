cab1947008902d1a33ce44ec427341fdca9e7ea0
fauzi2014444074 <paskal889@gmail.com>
2015-10-06 02:24:19 +0800

    update

--- Readtxt.php
+++ Readtxt.php
@@ -1,6 +1,6 @@
 <?php
 $myFile = "C:/Users/Fortesys/Desktop/AhmadFauzi.txt";
-$fh = fopen($myFile, 'r');
+$fh = fopen($myFile, 'r')or die("Unable to open file!");
 $theData = fread($fh,filesize($myFile));
 fclose($fh);
 echo $theData;
